Fix prop_pos_wrt_robot calculation in obs buffer
Resolve an issue where prop_pos_wrt_robot was only calculated for the first environment due to a misplaced for loop, leading to zero values being assigned in subsequent environments. This update ensures accurate calculations across all environments.

isaacgymenvs/tasks/a1_with_shovel_dagger_passive_joint.py
@@ -334,8 +334,7 @@ class A1WithShovelDaggerPassiveJoint(VecTask):
         base_pos = self.a1_root_states[:, 0:3].squeeze().cpu().numpy()
         base_ori = self.a1_root_states[:, 3:7].squeeze().cpu().numpy()
 
-        global_position = torch.zeros(self.num_envs, 3, dtype=torch.float, device=self.device, requires_grad=False)
-        local_position = torch.zeros(self.num_envs, 3, dtype=torch.float, device=self.device, requires_grad=False)
+        result = torch.zeros(self.num_envs, 3, dtype=torch.float, device=self.device, requires_grad=False)
 
         if (self.num_envs == 1):
             base_pos = base_pos.reshape((1, *base_pos.shape))
@@ -346,16 +345,14 @@ class A1WithShovelDaggerPassiveJoint(VecTask):
             transform_inverse = transform.inverse()
             if point_local is not None:
                 transformed_position = transform.transform_point(gymapi.Vec3(point_local[i][0], point_local[i][1], point_local[i][2]))
-                global_position[i] = torch.tensor([[transformed_position.x, transformed_position.y, transformed_position.z]],  dtype=torch.float, device=self.device, requires_grad=False)
-                return global_position
+                result[i] = torch.tensor([[transformed_position.x, transformed_position.y, transformed_position.z]],  dtype=torch.float, device=self.device, requires_grad=False)
             if vector_local is not None:
                 transformed_vector = transform.transform_vector(gymapi.Vec3(vector_local[i][0], vector_local[i][1], vector_local[i][2]))
-                global_position[i] = torch.tensor([[transformed_vector.x, transformed_vector.y, transformed_vector.z]],  dtype=torch.float, device=self.device, requires_grad=False)
-                return global_position
+                result[i] = torch.tensor([[transformed_vector.x, transformed_vector.y, transformed_vector.z]],  dtype=torch.float, device=self.device, requires_grad=False)
             if point_global is not None:
                 transformed_position = transform_inverse.transform_point(gymapi.Vec3(point_global[i][0], point_global[i][1], point_global[i][2]))
-                local_position[i] = torch.tensor([[transformed_position.x, transformed_position.y, transformed_position.z]],  dtype=torch.float, device=self.device, requires_grad=False)
-                return local_position
+                result[i] = torch.tensor([[transformed_position.x, transformed_position.y, transformed_position.z]],  dtype=torch.float, device=self.device, requires_grad=False)
+        return result
 
     def compute_observations(self):
         base_pos = self.a1_root_states[:, 0:3]
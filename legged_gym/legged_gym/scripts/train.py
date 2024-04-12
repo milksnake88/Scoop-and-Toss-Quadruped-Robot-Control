cf7dd47162ad1e29b54ff457622c2c2386e3c70f
tth-97 <alswl7763@hanyang.ac.kr>
2024-04-12 09:36:03 +0900

    Update entity in wandb init

--- legged_gym/legged_gym/scripts/train.py
+++ legged_gym/legged_gym/scripts/train.py
@@ -39,8 +39,12 @@ from shutil import copyfile
 import torch
 import wandb
 
+os.environ['MESA_VK_DEVICE_SELECT'] = '10de:2230'
+os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
 def train(args):
-    #args.headless = True
+    args.headless = True
     log_pth = LEGGED_GYM_ROOT_DIR + "/logs/{}/".format(args.proj_name) + args.exptid
     try:
         os.makedirs(log_pth)
@@ -56,7 +60,7 @@ def train(args):
 
     if args.no_wandb:
         mode = "disabled"
-    wandb.init(project=args.proj_name, name=args.exptid, entity="parkour", group=args.exptid[:3], mode=mode, dir="../../logs")
+    wandb.init(project=args.proj_name, name=args.exptid, entity="alswl7763", group=args.exptid[:3], mode=mode, dir="../../logs")
     wandb.save(LEGGED_GYM_ENVS_DIR + "/base/legged_robot_config.py", policy="now")
     wandb.save(LEGGED_GYM_ENVS_DIR + "/base/legged_robot.py", policy="now")
 

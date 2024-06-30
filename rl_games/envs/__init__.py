3f212e4395cc3b15cf68f174ddcea986f4d26094
강민지 <alswl7763@hanyang.ac.kr>
2024-06-30 18:43:53 +0900

    create cnn_mlp network

--- rl_games/envs/__init__.py
+++ rl_games/envs/__init__.py
@@ -3,7 +3,8 @@
 from rl_games.envs.connect4_network import ConnectBuilder
 from rl_games.envs.test_network import TestNetBuilder
 from rl_games.algos_torch import model_builder
-
+from rl_games.envs.cnn_mlp_network import CNNMLPNetBuilder
 
 model_builder.register_network('connect4net', ConnectBuilder)
 model_builder.register_network('testnet', TestNetBuilder)
+model_builder.register_network('cnn_mlp_network', CNNMLPNetBuilder)

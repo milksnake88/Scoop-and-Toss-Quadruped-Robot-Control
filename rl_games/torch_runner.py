ec0f4081e916cadf6fd270fc1720c9c04fbcf427
강민지 <alswl7763@hanyang.ac.kr>
2024-07-20 15:46:03 +0900

    create a2c_continuous_moe model

--- rl_games/torch_runner.py
+++ rl_games/torch_runner.py
@@ -9,6 +9,7 @@ from rl_games.common import object_factory
 from rl_games.common import tr_helpers
 
 from rl_games.algos_torch import a2c_continuous
+from rl_games.algos_torch import a2c_continuous_moe
 from rl_games.algos_torch import a2c_discrete
 from rl_games.algos_torch import players
 from rl_games.common.algo_observer import DefaultAlgoObserver
@@ -35,7 +36,8 @@ class Runner:
     def __init__(self, algo_observer=None):
         self.algo_factory = object_factory.ObjectFactory()
         self.algo_factory.register_builder('a2c_continuous', lambda **kwargs : a2c_continuous.A2CAgent(**kwargs))
-        self.algo_factory.register_builder('a2c_discrete', lambda **kwargs : a2c_discrete.DiscreteA2CAgent(**kwargs)) 
+        self.algo_factory.register_builder('a2c_continuous_moe', lambda **kwargs : a2c_continuous_moe.A2CMoEAgent(**kwargs))
+        self.algo_factory.register_builder('a2c_discrete', lambda **kwargs : a2c_discrete.DiscreteA2CAgent(**kwargs))
         self.algo_factory.register_builder('sac', lambda **kwargs: sac_agent.SACAgent(**kwargs))
         #self.algo_factory.register_builder('dqn', lambda **kwargs : dqnagent.DQNAgent(**kwargs))
 

1bd5c5ecfbe9c89b04a554fa5bc8f75df91274c4
강민지 <alswl7763@hanyang.ac.kr>
2024-08-03 17:00:38 +0900

    add agating network that output two dim action

--- rl_games/envs/__init__.py
+++ rl_games/envs/__init__.py
@@ -6,9 +6,11 @@ from rl_games.algos_torch import model_builder
 from rl_games.envs.cnn_mlp_network import CNNMLPNetBuilder
 from rl_games.envs.cnn_gating_network import CnnGatingNetBuilder
 from rl_games.envs.gating_network import GatingNetBuilder
+from rl_games.envs.gating_network_two_actions import GatingNetTwoActionsBuilder
 
 model_builder.register_network('connect4net', ConnectBuilder)
 model_builder.register_network('testnet', TestNetBuilder)
 model_builder.register_network('cnn_mlp_network', CNNMLPNetBuilder)
 model_builder.register_network('cnn_gating_network', CnnGatingNetBuilder)
 model_builder.register_network('gating_network', GatingNetBuilder)
+model_builder.register_network('gating_network_two_actions', GatingNetTwoActionsBuilder)

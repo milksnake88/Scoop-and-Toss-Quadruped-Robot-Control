53bf00b02ea54299701128840dd2c94f0e939a6e
강민지 <alswl7763@hanyang.ac.kr>
2024-09-24 22:38:13 +0900

    Create meta_controller and helper algo and register on algo builder

--- rl_games/torch_runner.py
+++ rl_games/torch_runner.py
@@ -13,6 +13,7 @@ from rl_games.algos_torch import a2c_continuous_moe
 from rl_games.algos_torch import a2c_continuous_moe_two_actions
 from rl_games.algos_torch import a2c_discrete_moe_two_actions
 from rl_games.algos_torch import a2c_discrete
+from rl_games.algos_torch import a2c_continuous_meta_controller_helper
 from rl_games.algos_torch import players
 from rl_games.algos_torch import players_moe
 from rl_games.algos_torch import players_moe_two_actions
@@ -47,6 +48,7 @@ class Runner:
         self.algo_factory.register_builder('a2c_discrete', lambda **kwargs : a2c_discrete.DiscreteA2CAgent(**kwargs))
         self.algo_factory.register_builder('sac', lambda **kwargs: sac_agent.SACAgent(**kwargs))
         #self.algo_factory.register_builder('dqn', lambda **kwargs : dqnagent.DQNAgent(**kwargs))
+        self.algo_factory.register_builder('a2c_continuous_meta_controller_helper', lambda **kwargs : a2c_continuous_meta_controller_helper.A2CContinuousMetaControllerHelperAgent(**kwargs))
 
         self.player_factory = object_factory.ObjectFactory()
         self.player_factory.register_builder('a2c_continuous', lambda **kwargs : players.PpoPlayerContinuous(**kwargs))

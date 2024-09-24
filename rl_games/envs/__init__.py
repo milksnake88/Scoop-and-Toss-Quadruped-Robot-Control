83165f57514c2d03b145309447318dd5bf5cd5e0
강민지 <alswl7763@hanyang.ac.kr>
2024-09-25 00:28:39 +0900

    Create meta_controller and helper network and register network builder

--- rl_games/envs/__init__.py
+++ rl_games/envs/__init__.py
@@ -7,6 +7,7 @@ from rl_games.envs.cnn_mlp_network import CNNMLPNetBuilder
 from rl_games.envs.cnn_gating_network import CnnGatingNetBuilder
 from rl_games.envs.gating_network import GatingNetBuilder
 from rl_games.envs.gating_network_two_actions import GatingNetTwoActionsBuilder
+from rl_games.envs.meta_controller_helper_network import MetaControllerHelperNetBuilder
 
 model_builder.register_network('connect4net', ConnectBuilder)
 model_builder.register_network('testnet', TestNetBuilder)
@@ -14,3 +15,4 @@ model_builder.register_network('cnn_mlp_network', CNNMLPNetBuilder)
 model_builder.register_network('cnn_gating_network', CnnGatingNetBuilder)
 model_builder.register_network('gating_network', GatingNetBuilder)
 model_builder.register_network('gating_network_two_actions', GatingNetTwoActionsBuilder)
+model_builder.register_network('meta_controller_helper_network', MetaControllerHelperNetBuilder)

bd463d773f91668258d24629d2df9df94e32106e
강민지 <alswl7763@hanyang.ac.kr>
2024-10-17 14:19:06 +0900

    [feat] Implement helper network

--- rl_games/envs/__init__.py
+++ rl_games/envs/__init__.py
@@ -8,6 +8,7 @@ from rl_games.envs.cnn_gating_network import CnnGatingNetBuilder
 from rl_games.envs.gating_network import GatingNetBuilder
 from rl_games.envs.gating_network_two_actions import GatingNetTwoActionsBuilder
 from rl_games.envs.meta_controller_helper_network import MetaControllerHelperNetBuilder
+from rl_games.envs.helper_network import HelperNetBuilder
 
 model_builder.register_network('connect4net', ConnectBuilder)
 model_builder.register_network('testnet', TestNetBuilder)
@@ -16,3 +17,4 @@ model_builder.register_network('cnn_gating_network', CnnGatingNetBuilder)
 model_builder.register_network('gating_network', GatingNetBuilder)
 model_builder.register_network('gating_network_two_actions', GatingNetTwoActionsBuilder)
 model_builder.register_network('meta_controller_helper_network', MetaControllerHelperNetBuilder)
+model_builder.register_network('helper_network', HelperNetBuilder)
